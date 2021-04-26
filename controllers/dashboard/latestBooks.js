e09e1e7a77b84aab152f919c5b2543de100f3652
goliakshay357 <goliakshay357@gmail.com>
2021-04-26 20:35:36 +0530

    Added dashboard and flex routes

---
+++ controllers/dashboard/latestBooks.js
@@ -0,0 +1,16 @@
+// Import DB model
+const { Books } = require("../../models/db");
+// Import status
+const { httpStatus200, httpStatus500 } = require("../../status/httpStatus");
+
+exports.latestBooks = async(req, res) => {
+    try {
+        // Getting the top 10 latest upload
+        const data = await Books.find().sort({ created_on: -1 }).limit(10);
+        res.status(200).json(httpStatus200(data, "Getting latest top 10 uploaded books"));
+    } catch (error) {
+        if (error) {
+            res.status(500).json(httpStatus500(error))
+        }
+    }
+}
