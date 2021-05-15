6947f14b9eaf08c9023954fb1d007f56c0df0e46
goliakshay357 <goliakshay357@gmail.com>
2021-05-15 11:47:37 +0530

    added the latest route changes

--- controllers/dashboard/latestBooks.js
+++ controllers/dashboard/latestBooks.js
@@ -6,10 +6,10 @@ const { httpStatus200, httpStatus500 } = require("../../status/httpStatus");
 exports.latestBooks = async(req, res) => {
     try {
         const limit_number = parseInt(req.params.number,10);
-
+        console.log(typeof limit_number, limit_number);
         // Getting the top 10 latest upload
         const data = await Books.find().sort({ created_on: -1 }).limit(limit_number);
-        return res.status(200).json(httpStatus200(data, "Getting latest top 10 uploaded books"));
+        return res.status(200).json(httpStatus200(data, `Getting latest top ${limit_number} uploaded books`));
     } catch (error) {
         if (error) {
             return res.status(500).json(httpStatus500(error))
